d968e338a18ebf79277a679a13f412dc5525e5bb
Kiril Vatev <vatev.1@gmail.com>
2019-02-25 21:19:39 -0500

    initial implementation

---
+++ index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const { spawn } = require('child_process');
+const argv = require('yargs-parser')(process.argv.slice(2));
+const importFrom = require('import-from');
+
+let executable = importFrom.silent(path.resolve('.'), 'electron');
+
+module.exports = () => {
+  let server;
+  const hook = path.resolve(__dirname, 'src/hook.js');
+
+  server = spawn(executable, [ `--require=${hook}` ].concat(argv._ || []), {
+    stdio: ['ignore', 'inherit', 'inherit'],
+    windowsHide: false
+  });
+
+  server.on('exit', (code) => {
+    if (server) {
+      console.log('server exited with code', code);
+      console.log('waiting for a change to restart it');
+    }
+
+    server = null;
+  });
+};
