19f0be5824c82625ebf6c4363167b25478c397fe
Kiril Vatev <vatev.1@gmail.com>
2020-05-01 19:02:41 -0400

    reading electronmon.patterns property from the local pacakge.json to augment patterns

--- src/package.js
+++ src/package.js
@@ -1,10 +1,20 @@
 const fs = require('fs');
 
-module.exports = (() => {
+const read = () => {
   try {
     const packageText = fs.readFileSync('./package.json', 'utf8');
     return JSON.parse(packageText || '{}');
   } catch (e) {
     return {};
   }
+};
+
+module.exports = (() => {
+  const pkg = read();
+
+  pkg.electronmon = Object.assign({
+    patterns: []
+  }, pkg.electronmon);
+
+  return pkg;
 })();
